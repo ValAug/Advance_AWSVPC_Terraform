0ea9da704c0c6d0161191cf0de93a6e740418687
AuVal <valdiviaaugusto9@gmail.com>
2021-02-03 19:18:38 -0500

    modified: networking/main.tf networking/variable.tf

--- networking/main.tf
+++ networking/main.tf
@@ -1,1 +1,8 @@
 # --- Networking module main
+
+resource "random_string" "vpc_rs" {
+    count = var.vpc_count
+    length = 4
+    special = true
+    upper = true  
+}
---
+++ networking/variable.tf
@@ -0,0 +1,7 @@
+# --- Networking module variables
+
+variable "vpc_count" {
+    description = "Numbers of vpc to be deploy with their random string names"
+    type = number
+  
+}
